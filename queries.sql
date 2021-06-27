a8e562f3f9b35c62f97e6b0d7e74479eefc850e5
Franwernke <franwernke@usp.br>
2021-06-26 22:46:55 -0300

    WIP: Add queries brainstorm

--- queries.sql
+++ queries.sql
@@ -1,15 +1,25 @@
 -- Lista todos os exames realizados, com seus respectivos tipos, bem como os seus
 -- usuários com suas respectivas datas de solicitação e execução.
 
-SELECT possui.cpf, exame.tipo, acessa.horario, amostra.data_da_coleta
-FROM possui
+SELECT paciente.cpf, exame.tipo, acessa.horario, amostra.data_da_coleta
+FROM paciente
 WHERE acessa.tipo='Solicitar exame';
 
 -- Liste os 5 exames realizados com maior eficiência (diferença entre data de execução e
 -- data de solicitação).
 
+SELECT *
+FROM exame
+ORDER BY amostra.data_da_coleta - acessa.horario 
+WHERE acessa.tipo='Solicitar exame', acessa.cpf=amostra.cpf
+LIMIT 5;
+
 -- Liste os serviços que podem ser utilizados por usuários tutelados para cada usuário
 -- tutor.
+
+SELECT usuario.nome, usuario.cpf, usuario_tutelado.nome, servico.tipo
+FROM usuario, servico, usuario_tutelado
+WHERE 
 
 -- Liste em ordem crescente o total de serviços utilizados agrupados pelos tipos de
 -- serviços disponíveis e pelo perfil dos usuários.

3a4a64c2ce069b4a5fbd79373ac91ebdecc7bd41
Vinicius Frota <viniciuspxfrota@gmail.com>
2021-07-15 21:42:50 -0300

    create main app

--- EP3/ep3/settings.py
+++ EP3/ep3/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f^+syg)&lv*cowd8k=l=omd@rhjja_d_3650@xln%d6+n316=y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
--- EP3/main/urls.py
+++ EP3/main/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
+from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    path('', lambda req: redirect('/main/')),
-    path('main/', include('main.urls'))
+    path('', views.index, name='index')
 ]
-
